f04a851f6ca4f21f186b28ecf5dc2930f0898380
guimartino <gui.martino@hotmail.com>
2017-05-21 22:07:44 -0300

    Lista de desejos finalizada

--- src/br/com/fiap/ManegedBean/GeralMB.java
+++ src/br/com/fiap/ManegedBean/GeralMB.java
@@ -1,13 +1,36 @@
 package br.com.fiap.ManegedBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.fiap.DAO.LivroDAO;
+import br.com.fiap.Model.Livro;
 
 @ManagedBean
+@SessionScoped
 public class GeralMB {
 
 	
 	private static String sucesso;
 	private static String erro;
+	private Livro livro;
+	private List<Livro> listaDesejos;
+
+	public GeralMB(){
+		livro = new Livro();
+		listaDesejos = new ArrayList<Livro>();
+	}
+	
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
 
 	public String getErro() {
 		return erro;
@@ -48,6 +71,44 @@ public class GeralMB {
 	public String abrirCarrinho(){
 		return "Carrinho";
 	}
+
+	public List<Livro> getListaDesejos() {
+		return listaDesejos;
+	}
+
+	public void setListaDesejos(List<Livro> listaDesejos) {
+		this.listaDesejos = listaDesejos;
+	}
 	
+	public String adicionarListaDeDesejos(){
+		LivroDAO dao = new LivroDAO();
+		livro = dao.pesquisarLivro(livro.getId());
+		listaDesejos.add(livro);
+		
+		return "ListaDesejo";
+	}
+	
+	public String adicionarCarrinho(){
+		CarrinhoMB cMB = new CarrinhoMB();
+		System.out.println(livro.getId() + " - " + livro.getNomeLivro());
+		cMB.setLivro(livro);
+		cMB.adcionarCarrinho();
+		removerListaDesejos();
+		
+		
+		return "Carrinho";
+	}
+	
+	public String removerListaDesejos(){
+		Livro l = new Livro();
+		for (int i = 0; i < listaDesejos.size(); i++) {
+			l = listaDesejos.get(i);
+			if(l.getId() == this.livro.getId()){
+				listaDesejos.remove(i);
+			}
+		}
+		
+		return "ListaDesejos";
+	}
 	
 }
